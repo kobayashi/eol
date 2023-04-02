1f4361d3a837b35ac11ce32df9896175778bccce
Jake Warren <jakewarren@users.noreply.github.com>
2023-04-02 13:56:20 -0500

    add shell completion to project command

--- cmd/project.go
+++ cmd/project.go
@@ -1,11 +1,12 @@
 package cmd
 
 import (
+	"context"
+
+	"github.com/kobayashi/eol/pkg/api"
 	"github.com/kobayashi/eol/pkg/commands"
 	"github.com/spf13/cobra"
 )
-
-var format string
 
 // projectCmd represents the project command
 var projectCmd = &cobra.Command{
@@ -16,6 +17,18 @@ var projectCmd = &cobra.Command{
 	},
 	RunE: func(cmd *cobra.Command, args []string) error {
 		return commands.RunGetProject(cmd, args)
+	},
+	ValidArgsFunction: func(cmd *cobra.Command, args []string, toComplete string) ([]string, cobra.ShellCompDirective) {
+		if len(args) != 0 {
+			return nil, cobra.ShellCompDirectiveNoFileComp
+		}
+		c := api.NewHTTPClient()
+		ctx := context.Background()
+		res, err := c.GetAll(ctx)
+		if err != nil {
+			return nil, cobra.ShellCompDirectiveNoFileComp
+		}
+		return res, cobra.ShellCompDirectiveNoFileComp
 	},
 }
 
